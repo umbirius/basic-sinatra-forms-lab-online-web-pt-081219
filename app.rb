5c6ba25200d677fe4ceb6ddd7b76ccd233780c5b
Umberto Palazzo <upalazzo93@gmail.com>
2019-11-13 19:58:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,13 +8,13 @@ class App < Sinatra::Base
   end 
   
   post '/team' do 
-    @name = params["name"]
-    @coach = params["coach"]
-    @pg = params["pg"]
-    @sg = params["sg"]
-    @sf = params["sf"]
-    @pf = params["pf"]
-    @c = params["c"]
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @sf = params[:sf]
+    @pf = params[:pf]
+    @c = params[:c]
     erb :team 
   end 
   
